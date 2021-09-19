c91224ebb040b93b8713e1f43e54637323cf7552
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2021-09-19 11:18:23 -0300

    GENESIS

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module br.com.fitcontrol.fitcontrol {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens br.com.fitcontrol.fitcontrol to javafx.fxml;
+    exports br.com.fitcontrol.fitcontrol;
+}
