3acf17a4918c8c85c7cbec68a12f98c458271efa
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2021-09-19 17:57:49 -0300

    Main screen initial layout done

--- src/main/java/br/com/fitcontrol/fitcontrol/controllers/MainScreenController.java
+++ src/main/java/br/com/fitcontrol/fitcontrol/controllers/MainScreenController.java
@@ -1,4 +1,18 @@
 package br.com.fitcontrol.fitcontrol.controllers;
 
-public class MainScreenController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainScreenController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+    }
+
+
 }
