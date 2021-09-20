6ad7de349ef97a7d58d0bb715b067caa1a2099a9
lucasscbr <55152520+lucasscbr@users.noreply.github.com>
2021-09-20 10:45:40 -0300

    Employees screen

--- src/main/java/br/com/fitcontrol/fitcontrol/controllers/EmployeesScreenController.java
+++ src/main/java/br/com/fitcontrol/fitcontrol/controllers/EmployeesScreenController.java
@@ -1,4 +1,18 @@
 package br.com.fitcontrol.fitcontrol.controllers;
 
-public class EmployeesScreenController {
+import br.com.fitcontrol.fitcontrol.FitControlMain;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class EmployeesScreenController extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(FitControlMain.class.getResource("employees-screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 1024);
+        primaryStage.setTitle("Fit Control");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
