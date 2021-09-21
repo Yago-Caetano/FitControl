9d0b7cdd33681626c9719f7b7f84f0da00707451
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2021-09-20 21:00:11 -0300

    Mouse Clicked event inserted

--- src/main/java/br/com/fitcontrol/fitcontrol/controllers/MainScreenController.java
+++ src/main/java/br/com/fitcontrol/fitcontrol/controllers/MainScreenController.java
@@ -1,18 +1,32 @@
 package br.com.fitcontrol.fitcontrol.controllers;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainScreenController implements Initializable {
 
+    @FXML
+    private HBox catracaHbox;
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
-
+        catracaHbox.addEventFilter(MouseEvent.MOUSE_CLICKED,catracaClicked);
     }
 
+    //Creating the mouse event handler
+    EventHandler<MouseEvent> catracaClicked = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+            System.out.println("Clicou na catraca");
+        }
+    };
 
 }
