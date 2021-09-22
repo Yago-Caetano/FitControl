59633bda272ef6adb786fc88a5edb84646cfba2a
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2021-09-22 20:46:38 -0300

    Navigation

--- src/main/java/br/com/fitcontrol/fitcontrol/FitControlMain.java
+++ src/main/java/br/com/fitcontrol/fitcontrol/FitControlMain.java
@@ -1,20 +1,39 @@
 package br.com.fitcontrol.fitcontrol;
 
+import br.com.fitcontrol.fitcontrol.navigation.NavigationSingleton;
+import br.com.fitcontrol.fitcontrol.navigation.iNavCallback;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class FitControlMain extends Application {
+
+    NavigationSingleton navigation;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        /*FXMLLoader fxmlLoader = new FXMLLoader(FitControlMain.class.getResource("main-screen.fxml"));*/
+        navigation = NavigationSingleton.getInstance();
+        navigation.setStage(primaryStage);
+        /*//FXMLLoader fxmlLoader = new FXMLLoader(FitControlMain.class.getResource("main-screen.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(FitControlMain.class.getResource("rewards-screen.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 1440, 1024);
         primaryStage.setTitle("Fit Control");
         primaryStage.setScene(scene);
+        primaryStage.show();*/
+        navigation.navigate(NavigationSingleton.MAIN_SCREEN, new iNavCallback() {
+            @Override
+            public void navigateCb(String screenName) throws IOException {
+                FXMLLoader fxmlLoader = new FXMLLoader(FitControlMain.class.getResource(screenName));
+                Scene scene = new Scene(fxmlLoader.load(), 1440, 1024);
+                primaryStage.setTitle("Fit Control");
+                primaryStage.setScene(scene);
+
+            }
+        });
         primaryStage.show();
     }
 }
